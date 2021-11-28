d7ec26c9784aea997e46870e9a1268c1a293297d
Joe Webber <joewebber@users.noreply.github.com>
2021-11-28 21:20:19 +0000

    Update homepage to serve as landing page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
-import Link from 'next/link'
-
 import Layout from '@components/Layout'
-import PostList from '@components/PostList'
 
-import getPosts from '@utils/getPosts'
-
-const Index = ({ posts, title, description, ...props }) => {
+const Index = ({ title, description }) => {
   return (
     <>
       <Layout pageTitle={title} description={description}>
-        <h1 className="title">I'm a Software Engineer. This is my blog</h1>
-        <h2>I write about stuff that interests me, and the occasional tutorial, mainly for my own benefit.
-          Topics include <Link href="/"><a>Cloud Computing</a></Link>, <Link href="/"><a>Docker</a></Link>, <Link href="/"><a>Javascript</a></Link>, <Link href="/"><a>Python</a></Link> and occassionally <Link href="/"><a>something more interesting</a></Link>.
-        During the day, I work on Cloud Applications built in Javascript and Python and deployed to Google Cloud. At night, I like to play Minecraft and tinker with code.</h2>
-        <main>
-          <PostList posts={posts} />
-        </main>
+        <h1 className="title">Software Engineer / Consultant</h1>
+        <h2>20 years building products for online and offline.<br />10 years managing teams of Engineers.</h2>
+        <h3>Node JS / React / Python / Docker / Google Cloud</h3>
+        <ul>
+          <li><a href="https://www.linkedin.com/in/joe-webber-18254b16/"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a></li>
+          <li><a href="https://github.com/joewebber"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg></a></li>
+        </ul>
       </Layout>
       <style jsx>{`
         .title {
@@ -24,8 +19,19 @@ const Index = ({ posts, title, description, ...props }) => {
         }
 
         h2 {
-          max-width: 800px;
           line-height: 2.4rem;
+          font-size: 1.6rem;
+        }
+
+        ul {
+          display: flex;
+          list-style: none;
+          margin: 0;
+          padding: 0;
+        }
+
+        li {
+          margin-right: 1.5rem;
         }
       `}</style>
     </>
@@ -33,20 +39,3 @@ const Index = ({ posts, title, description, ...props }) => {
 }
 
 export default Index
-
-export async function getStaticProps() {
-  const configData = await import(`../siteconfig.json`)
-
-  const data = await getPosts()
-  const posts = data.map((p) => {
-    return p.fields
-  })
-
-  return {
-    props: {
-      posts,
-      title: configData.default.title,
-      description: configData.default.description,
-    },
-  }
-}
